31577874c34268afc882bc81115d07c703c7204f
Rishav222222 <160915777+Rishav222222@users.noreply.github.com>
2024-06-26 19:42:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 function makeid(l) {
   // write your code here
+
+
+	const char = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	let res = '';
+
+	for(let i=0;i<l;i++){
+		const random = Math.floor(Math.random()*char.length);
+		res += char.charAt(random);
+	}
+	return res;
 }
 
 // Do not change the code below.
-const l = prompt("Enter a number.");
+	const l = prompt("Enter a number.");
 alert(makeid(l));
+
